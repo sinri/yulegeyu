7250eaa6c4f5e7490ac62659dbd3f9810a90438f
hellomrbigshot <1994yechen@gmail.com>
2022-10-14 13:12:12 +0800

    ✨ 添加 build.target 兼容 chrome64

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: 'chrome64',
+  },
   plugins: [
     vue({
       // 支持 Markdown 文件加载
