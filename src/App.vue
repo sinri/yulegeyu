5ab97016fea8755e4fb453f8ba951f7c14487061
Sinri Edogawa <ljni@leqee.com>
2022-10-14 12:23:15 +0800

    add eruda

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,14 @@
   </div>
 </template>
 <script setup lang="ts"></script>
+<script>
+  var script = document.createElement('script');
+  script.src = "./eruda.js";
+  document.body.appendChild(script);
+  script.onload = function () {
+    eval('eruda.init()');
+  }
+</script>
 <style scoped>
 #app {
   background: url("assets/bg.jpeg");
